d4baa02d012e370df302af09f37033cb817d166e
chanki <chanki9507@gmail.com>
2023-07-03 18:17:34 +0900

    Initial commit

---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,27 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNull;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void equalTest(){
+		assertEquals("",1,1);
+	    //given
+	    //when
+	    //then
+	}
+	@Test
+	void nullTest() {
+		assertNull("",null);
+	    //given
+	    //when
+	    //then
+	}
+
+}
